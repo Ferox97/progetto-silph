a23a5c89038e2d0d5786d109797e427189f663fe
Ferox97 <piccolofrancesco97@gmail.com>
2019-06-08 15:49:45 +0200

    up3

--- azienda-silph/src/main/java/it/uniroma3/siw/repository/FotografiaRepository.java
+++ azienda-silph/src/main/java/it/uniroma3/siw/repository/FotografiaRepository.java
@@ -1,7 +1,18 @@
 package it.uniroma3.siw.repository;
 
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.siw.model.Fotografia;
+
+
 public class FotografiaRepository {
 
-	
+	public interface StudenteRepository extends CrudRepository<Fotografia,Long> {
+		//findByDatiDaRecuperare(sarebbero le variabili)
+		public List<Fotografia> findByNome(String nome);
+		public List<Fotografia> findByNomeAndCognome(String nome, String cognome);
+	}
 	
 }
--- azienda-silph/src/main/java/it/uniroma3/siw/service/FotografiaService.java
+++ azienda-silph/src/main/java/it/uniroma3/siw/service/FotografiaService.java
@@ -1,9 +1,17 @@
 package it.uniroma3.siw.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import it.uniroma3.siw.model.Fotografia;
 import it.uniroma3.siw.repository.FotografiaRepository;
+
+
+import java.util.Optional;
+
 
 @Service
 public class FotografiaService {
@@ -11,7 +19,15 @@ public class FotografiaService {
 	@Autowired
 	private FotografiaRepository fotografiaRepository;
 	
+	@Transactional 
+	public Fotografia inserisci(Fotografia fotografia) {
+		return fotografiaRepository.save(fotografia);
+	}
 	
+	@Transactional
+	public List<Fotografia> tutti(){
+		return (List<Fotografia>) fotografiaRepository.findAll();
+	}
 	
 	
 }
