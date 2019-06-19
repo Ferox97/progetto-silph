d91b62ea0d72d51cf671514ac9a6943b1ab4d1a1
Ferox97 <piccolofrancesco97@gmail.com>
2019-06-19 11:57:34 +0200

    2.0

--- azienda-silph/src/main/resources/import.sql
+++ azienda-silph/src/main/resources/import.sql
@@ -8,22 +8,22 @@ INSERT INTO fotografo (id , nome, cognome) VALUES (4 ,'Francesco' , 'Piccolo' );
 
 INSERT INTO fotografo (id , nome, cognome) VALUES (5 , 'Luigi' , 'Zicchittu' );
 
-INSERT INTO fotografo (id , nome, descrizione , album , fotografo ) VALUES ('natura1.jpg' , 'Montagna' , 'Prato e Montagna' , 2 , 5 );
+INSERT INTO fotografia (id , nome, descrizione , album_id , fotografo_id ) VALUES ('natura1.jpg' , 'Montagna' , 'Prato e Montagna' , 2 , 5 );
 
-INSERT INTO fotografo (id , nome, descrizione , album , fotografo ) VALUES ('natura2.jpg' , 'Tridente Marino' , 'Ade affiora' , 2 , 5 );
+INSERT INTO fotografia (id , nome, descrizione , album_id , fotografo_id ) VALUES ('natura2.jpg' , 'Tridente Marino' , 'Ade affiora' , 2 , 5 );
 
-INSERT INTO fotografo (id , nome, descrizione , album , fotografo ) VALUES ('natura3.jpg' , 'Torrente' , 'La vita nel Deserto' , 2 , 5 );
+INSERT INTO fotografia (id , nome, descrizione , album_id , fotografo_id ) VALUES ('natura3.jpg' , 'Torrente' , 'La vita nel Deserto' , 2 , 5 );
 
-INSERT INTO fotografo (id , nome, descrizione , album , fotografo ) VALUES ('natura4.jpg' , 'Quiete e Tempesta' , 'Paesaggio Scuro' , 2 , 5 );
+INSERT INTO fotografia (id , nome, descrizione , album_id , fotografo_id ) VALUES ('natura4.jpg' , 'Quiete e Tempesta' , 'Paesaggio Scuro' , 2 , 5 );
 
-INSERT INTO fotografo (id , nome, descrizione , album , fotografo ) VALUES ('natura5.jpg' , 'Iceberg' , 'Pericolo affioramento' , 2 , 5 );
+INSERT INTO fotografia (id , nome, descrizione , album_id , fotografo_id ) VALUES ('natura5.jpg' , 'Iceberg' , 'Pericolo affioramento' , 2 , 5 );
 
-INSERT INTO fotografo (id , nome, descrizione , album , fotografo ) VALUES ('sunset1.jpg' , 'Tramonto Oscuro' , 'Un tramonto nella Notte' , 3 , 4 );
+INSERT INTO fotografia (id , nome, descrizione , album_id , fotografo_id ) VALUES ('sunset1.jpg' , 'Tramonto Oscuro' , 'Un tramonto nella Notte' , 3 , 4 );
 
-INSERT INTO fotografo (id , nome, descrizione , album , fotografo ) VALUES ('sunset2.jpg' , 'Tramonto nel Fiume' , 'Un bel tramonto sulle sponde della vita' , 3 , 4 );
+INSERT INTO fotografia (id , nome, descrizione , album_id , fotografo_id ) VALUES ('sunset2.jpg' , 'Tramonto nel Fiume' , 'Un bel tramonto sulle sponde della vita' , 3 , 4 );
 
-INSERT INTO fotografo (id , nome, descrizione , album , fotografo ) VALUES ('sunset3.jpg' , 'Barca e Tramonto' , 'Una barca costeggia un tramonto' , 3 , 4 );
+INSERT INTO fotografia (id , nome, descrizione , album_id , fotografo_id ) VALUES ('sunset3.jpg' , 'Barca e Tramonto' , 'Una barca costeggia un tramonto' , 3 , 4 );
 
-INSERT INTO fotografo (id , nome, descrizione , album , fotografo ) VALUES ('sunset4.jpg' , 'Baia al Tramonto' , 'Casette in riva al mare' , 3 , 4 );
+INSERT INTO fotografia (id , nome, descrizione , album_id , fotografo_id ) VALUES ('sunset4.jpg' , 'Baia al Tramonto' , 'Casette in riva al mare' , 3 , 4 );
 
-INSERT INTO fotografo (id , nome, descrizione , album , fotografo ) VALUES ('sunset5.jpg' , 'Roccaforte al Tramonto' , 'L'antichità si colora di rosso' , 3 , 4 );
+INSERT INTO fotografia (id , nome, descrizione , album_id , fotografo_id ) VALUES ('sunset5.jpg' , 'Roccaforte al Tramonto' , 'Le rovine si colorano di rosso' , 3 , 4 );
